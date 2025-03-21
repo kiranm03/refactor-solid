55c714dff05e868752383ec1a3e3cc7e2346d5ae
kiran <kiran>
2025-03-21 23:32:08 +1100

    Move DI classes to separate files

--- MegaPricer/Services/PricingService.cs
+++ MegaPricer/Services/PricingService.cs
@@ -321,31 +321,3 @@ public class PricingService(IUserMarkup userMarkup) : IPricingService
     return dt;
   }
 }
-
-public interface IUserMarkup
-{
-  decimal GetUserMarkup(string userName);
-}
-
-public class SqliteUserMarkup : IUserMarkup
-{
-  public decimal GetUserMarkup(string userName)
-  {
-    decimal userMarkup = 0;
-    using (var conn = new SqliteConnection(ConfigurationSettings.ConnectionString))
-    {
-      var cmd = conn.CreateCommand();
-      cmd.CommandText = "SELECT * FROM UserMarkups WHERE UserName = @userName";
-      cmd.Parameters.AddWithValue("@userName", userName);
-      conn.Open();
-      using (SqliteDataReader dr = cmd.ExecuteReader())
-      {
-        if (dr.HasRows && dr.Read())
-        {
-          userMarkup = dr.GetDecimal("MarkupPercent");
-        }
-      }
-    }
-    return userMarkup;
-  }
-}
---
+++ MegaPricer/Services/SqliteUserMarkup.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using MegaPricer.Data;
+using Microsoft.Data.Sqlite;
+
+namespace MegaPricer.Services;
+
+public class SqliteUserMarkup : IUserMarkup
+{
+  public decimal GetUserMarkup(string userName)
+  {
+    decimal userMarkup = 0;
+    using (var conn = new SqliteConnection(ConfigurationSettings.ConnectionString))
+    {
+      var cmd = conn.CreateCommand();
+      cmd.CommandText = "SELECT * FROM UserMarkups WHERE UserName = @userName";
+      cmd.Parameters.AddWithValue("@userName", userName);
+      conn.Open();
+      using (SqliteDataReader dr = cmd.ExecuteReader())
+      {
+        if (dr.HasRows && dr.Read())
+        {
+          userMarkup = dr.GetDecimal("MarkupPercent");
+        }
+      }
+    }
+    return userMarkup;
+  }
+}
